f65624f0163b0b128d0350b5b3e22003fbee67ef
alexabotello <abotello20@yahoo.com>
2021-04-21 16:17:48 -0500

    html & css complete

---
+++ app.js
@@ -0,0 +1,10 @@
+const userScore = o;
+const computerScore = 0;
+// DOM variables 
+const userScore_span = document.getElementById("user-score"); 
+const computerScore_span = document.getElementById("computer-score"); 
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock"); 
+const paper_div = document.getElementById("paper"); 
+const scissors_div = document.getElementById("scissors"); 
