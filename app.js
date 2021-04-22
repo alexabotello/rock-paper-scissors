5f72cf56702e3576177071254b5a45186cf4c732
alexabotello <abotello20@yahoo.com>
2021-04-22 01:45:01 -0500

    js functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const userScore = o;
+const userScore = 0;
 const computerScore = 0;
 // DOM variables 
 const userScore_span = document.getElementById("user-score"); 
@@ -8,3 +8,30 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock"); 
 const paper_div = document.getElementById("paper"); 
 const scissors_div = document.getElementById("scissors"); 
+
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+console.log(getComputerChoice());
+
+function game(userChoice) {
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+main();
